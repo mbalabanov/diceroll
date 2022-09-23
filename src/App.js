966e81f45ddb58833ab1d070a5f8bb6a15cb70c9
mbalabanov <marincomics@gmail.com>
2022-09-23 15:30:10 +0300

    Adds initial solution

--- src/App.js
+++ src/App.js
@@ -1,22 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+
+import "./App.css";
 
 function App() {
+  const diceNumbers = {
+    1: "⚀",
+    2: "⚁",
+    3: "⚂",
+    4: "⚃",
+    5: "⚄",
+    6: "⚅",
+  };
+
+  const emojiReactions = {
+    1: "😭",
+    2: "🙃",
+    3: "😀",
+    4: "🤩",
+    5: "🤣",
+    6: "🔥",
+  };
+
+  const [diceState, setDiceState] = useState([5, 5]);
+
+  const rollDice = (event) => {
+    const firstDie = Math.floor(Math.random() * 6) + 1;
+    const secondDie = Math.floor(Math.random() * 6) + 1;
+    setDiceState([firstDie, secondDie]);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <h1>Dice roll</h1>
+        <p className="diceNumber">
+          <span className="eachReactionEmoji">
+            {emojiReactions[diceState[0]]}
+          </span>
+          {diceNumbers[diceState[0]]}
+          {diceNumbers[diceState[1]]}
+          <span className="eachReactionEmoji">
+            {emojiReactions[diceState[1]]}
+          </span>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        <button className="buttonRollDice" onClick={rollDice}>
+          Roll Die
+        </button>
       </header>
     </div>
   );
